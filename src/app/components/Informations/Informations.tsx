7b9d84d7b3ca65d1212a9fff372c7edb6b1e70d0
Islam Mokrane <islammkr01@gmail.com>
2024-09-29 17:46:07 +0200

    feat: refactor Informations component and add interactive sections and links

--- src/app/components/Informations/Informations.tsx
+++ src/app/components/Informations/Informations.tsx
@@ -1,21 +1,59 @@
+"use client"
+
+import { useState } from "react"
 import Grid from "@mui/material/Grid2"
-import Image from "next/image"
 import styles from "./informations.module.css"
 import GithubSVG from "../../assets/github.svg"
 import LinkedInSVG from "../../assets/linkedin.svg"
 
 const Informations = () => {
+	const [activeSection, setActiveSection] = useState<"about" | "experiences" | "projects">("about")
+
+	const openMyGithubPage = () => {
+		window.open("https://github.com/IslamMkr", "_blank")
+	}
+
+	const openMyLinkedInPage = () => {
+		window.open("https://www.linkedin.com/in/islam-mokrane/", "_blank")
+	}
+
 	return (
 		<Grid className={styles.informations}>
-			<Grid>
-				<p className={styles.name}>Islam Mokrane</p>
-				<p className={styles.occupation}>Junior Software Engineer</p>
-				<p className={styles.introduction}>I build pixel-perfect, engaging, and accessible digital experiences.</p>
+			<Grid className={styles.upperSection}>
+				<Grid>
+					<p className={styles.name}>Islam Mokrane</p>
+					<p className={styles.occupation}>Junior Software Engineer</p>
+					<p className={styles.introduction}>
+						Crafting high-performance, intuitive digital solutions while ensuring quality, accessibility, and seamless
+						user experiences.
+					</p>
+				</Grid>
+
+				<Grid className={styles.sections}>
+					<Grid
+						className={`${styles.section} ${activeSection === "about" ? styles.active : ""}`}
+						onClick={() => setActiveSection("about")}>
+						<hr className={styles.sectionLine} />
+						<p className={styles.sectionTitle}>ABOUT</p>
+					</Grid>
+					<Grid
+						className={`${styles.section} ${activeSection === "experiences" ? styles.active : ""}`}
+						onClick={() => setActiveSection("experiences")}>
+						<hr className={styles.sectionLine} />
+						<p className={styles.sectionTitle}>EXPERIENCES</p>
+					</Grid>
+					<Grid
+						className={`${styles.section} ${activeSection === "projects" ? styles.active : ""}`}
+						onClick={() => setActiveSection("projects")}>
+						<hr className={styles.sectionLine} />
+						<p className={styles.sectionTitle}>PROJECTS</p>
+					</Grid>
+				</Grid>
 			</Grid>
 
-			<Grid className={styles.links} style={{ marginTop: 30 }}>
-				<Image src={GithubSVG} alt="Github" width={24} height={24} />
-				<Image src={LinkedInSVG} alt="LinkedIn" width={24} height={24} />
+			<Grid className={styles.links}>
+				<GithubSVG alt="Github" className={styles.svg} onClick={openMyGithubPage} />
+				<LinkedInSVG alt="LinkedIn" className={styles.svg} onClick={openMyLinkedInPage} />
 			</Grid>
 		</Grid>
 	)
